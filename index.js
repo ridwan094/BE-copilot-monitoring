e3f495768ff17fbc34b1253266d1ee575932ccb0
ridwan094 <ridwansaefudin97@gmail.com>
2024-07-23 14:01:58 +0700

    api-monitoring-copilot

--- index.js
+++ index.js
@@ -1,47 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const sequelize = require('./config/database');
-// const userBridevRoutes = require('./routes/userBridevRoute');
-// const logCopilotRoutes = require('./routes/logCopilotRoute');
-// const authRoutes = require('./routes/authRoute');
-// const logDailyBridevRoute = require('./routes/logDailyBridevRoute');
-// const logSummaryRoute = require('./routes/logSummaryRoute');
-// const userRoutes = require('./routes/userRoute');
-// require('dotenv').config();
-
-// const app = express();
-// const PORT = 3000;
-
-// app.use(cors({
-//   origin: 'https://fe-monitoring-copilot-k4sb3rrmz-oktavianbayuws-projects.vercel.app:3001',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization',  'Insert-Type']
-// }));
-
-
-// app.use(bodyParser.json());
-// app.use('/api', [
-//   userRoutes,
-//   authRoutes,
-//   userBridevRoutes, 
-//   logCopilotRoutes,
-//   logDailyBridevRoute,
-//   logSummaryRoute
-// ]);
-
-// sequelize.sync()
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-
-// perubahan 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -55,15 +11,16 @@ const userRoutes = require('./routes/userRoute');
 require('dotenv').config();
 
 const app = express();
+const PORT = 3000;
 
 app.use(cors({
-  origin: '*', // Anda bisa mengganti dengan origin yang diperlukan
+  origin: 'http://localhost:3001',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Insert-Type']
+  allowedHeaders: ['Content-Type', 'Authorization',  'Insert-Type']
 }));
 
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 app.use('/api', [
   userRoutes,
   authRoutes,
@@ -73,6 +30,12 @@ app.use('/api', [
   logSummaryRoute
 ]);
 
-app.get('/', (req, res) => res.send('Hello World'));
-
-module.exports = app;
+sequelize.sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
